eeb0b6c34414fe16eafd6a9d231db142b6943d69
Paulo Lima <paulo.lima@zrp.com.br>
2021-12-13 14:11:34 -0300

    fix: handle syntax error on calculate display value

--- js/index.js
+++ js/index.js
@@ -9,13 +9,18 @@ function clearDisplayValue() {
 function calculateValue() {
   const displayValue = document.getElementById("container-display").value;
 
-  if (displayValue !== '') {
-    const totalValue = eval(displayValue);
+  if (displayValue !== '' && displayValue !== 'Sintax error') {
+    try {
+      const totalValue = eval(displayValue);
     
-    if (Number.isInteger(totalValue)) {
-      document.getElementById("container-display").value = totalValue;
-    } else {
-      document.getElementById("container-display").value = totalValue.toFixed(6);
+      if (Number.isInteger(totalValue)) {
+        document.getElementById("container-display").value = totalValue;
+      } else {
+        document.getElementById("container-display").value = totalValue.toFixed(6);
+      }
+    } catch (error) {
+      console.log("Syntax error: ", error);
+      document.getElementById("container-display").value = "Sintax error";
     }
   }
 }
